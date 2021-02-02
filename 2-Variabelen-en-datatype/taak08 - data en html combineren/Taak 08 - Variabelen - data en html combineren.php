1f7e28380da283dcfe5db0056b39f95f9edebc0c
dylanvaneijk <2114448@talnet.nl>
2021-02-02 15:39:41 +0100

    klaar

---
+++ 2-Variabelen-en-datatype/taak08 - data en html combineren/Taak 08 - Variabelen - data en html combineren.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+    <?php
+           $voornaam1 = "dylan";
+           $voornaam2 = "bart";
+           $voornaam3 = "gyani";
+          
+              echo ("<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>");
+
+     ?>
+
+  </body>
+</html>
