019c409f91b9c880688982a94e9ce9b92503d964
dylanvaneijk <2114448@talnet.nl>
2021-02-02 14:06:44 +0100

    klaar

---
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/Taak 06 - Datatypes - tonen op het scherm met gettype().php
@@ -0,0 +1,18 @@
+<?php
+$hallo = "hoi";
+$punten = true;
+$geld = 5958;
+$punten = 10.23;
+$cars = null;
+
+echo gettype($hallo);
+
+echo gettype($punten);
+
+echo gettype($geld);
+
+echo gettype($punten);
+
+echo gettype($cars);
+
+ ?>
