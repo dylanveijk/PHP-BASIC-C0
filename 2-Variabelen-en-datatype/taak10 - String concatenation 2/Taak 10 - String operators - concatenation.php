ba6ad32c5320077096b4dfba3fbb82b38c3b3a7a
dylanvaneijk <2114448@talnet.nl>
2021-02-02 22:29:23 +0100

    done

---
+++ 2-Variabelen-en-datatype/taak10 - String concatenation 2/Taak 10 - String operators - concatenation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $item = "<li>tas</li>";
+      $item .= "<li>bal</li><li>laptop</li>";
+echo $item;
+     ?>
+
+  </body>
+</html>
