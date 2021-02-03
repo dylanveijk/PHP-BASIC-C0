8c7d6de075313ad088357a00baf896cfac44732c
dylanvaneijk <2114448@talnet.nl>
2021-02-03 20:27:44 +0100

    done

---
+++ 3-Arrays/taak04 - Multidimensionaal/Taak 04 - Multidimensionaal.php
@@ -0,0 +1,15 @@
+<?php
+
+$abcGetallen = [["a","b","c","d"],"24,56,78,90"];
+
+print_r($abcGetallen);
+
+echo $abcGetallen[0][0];
+echo $abcGetallen[1][3];
+echo $abcGetallen[1][4];
+
+echo $abcGetallen[0][2];
+echo $abcGetallen[1][9];
+echo $abcGetallen[1][10];
+
+ ?>
