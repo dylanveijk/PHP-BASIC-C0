d9569a7398b84aacf8cd92e254e17280a696b631
dylanvaneijk <2114448@talnet.nl>
2021-02-07 16:36:13 +0100

    done

---
+++ 5-Conditional_Statements/taak02 - if conditional/Taak 02 - Conditional statement - if.php
@@ -0,0 +1,42 @@
+
+    <?php
+
+    $test1 = 10;
+    $test2 = 6;
+
+
+    if($test1 == $test2) {
+        echo "gelijk";
+    }
+
+    if($test1 != $test2) {
+        echo "<li>ongelijk</li>";
+    }
+
+
+        if($test1 === $test2) {
+            echo "identiek";
+        }
+
+            if($test1 > $test2) {
+                echo "<li>test1 is groter dan test2</li>";
+            }
+
+
+                if($test1 < $test2) {
+                    echo "<li>test1 is kleiner dan test2</li>";
+                }
+
+
+                    if($test1 >= $test2) {
+                        echo "<li>test1 is groter of gelijk aan test2</li>";
+                    }
+
+
+                        if($test1 <= $test2) {
+                            echo "<li>test1 is kleiner of gelijk aan test2</li>";
+                        }
+
+
+
+     ?>
