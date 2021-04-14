5cfdf97d9ec2e5916637aef70c5d07b8178b5257
dylanvaneijk <2114448@talnet.nl>
2021-04-14 14:23:41 +0200

    done

---
+++ 5-Conditional_Statements/taak05 - switch conditional/Taak 05 - Conditional statement - Switch.php
@@ -0,0 +1,110 @@
+<?php
+ $score = 10;
+ $score1 = 9;
+ $score2 = 8;
+ $score3 = 7;
+ $score4 = 5;
+
+
+
+switch($score) {
+    case 10:
+    echo "<li>Wow wat een fantastische score!</li>";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+
+
+
+switch($score1) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "<li>Wow bijna alle punten nog 1 te gaan!</li>";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+
+
+switch($score2) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "<li>Goed bezig, bijna alle punten!</li>";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+
+
+switch($score3) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "<li>Ga zo door!</li>";
+    break;
+    default:
+    echo "Nog even oefenen dan haal je vast alle punten!";
+}
+
+
+
+switch($score4) {
+    case 10:
+    echo "Wow wat een fantastische score!";
+    break;
+    case 9:
+    echo "Wow bijna alle punten nog 1 te gaan!";
+    break;
+    case 8:
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 7:
+    echo "Ga zo door!";
+    break;
+    default:
+    echo "<li>Nog even oefenen dan haal je vast alle punten!</li>";
+}
+
+
+
+
+
+
+
+ ?>
